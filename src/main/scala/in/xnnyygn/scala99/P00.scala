0cd60205f1d76e24a09310805498b77829118a85
xnnyygn <xnnyygn@gmail.com>
2015-02-10 10:01:08 +0800

    add P00

---
+++ src/main/scala/in/xnnyygn/scala99/P00.scala
@@ -0,0 +1,7 @@
+package in.xnnyygn.scala99
+
+// a template for answer
+object P00 {
+
+  // method goes here
+}
---
+++ src/test/scala/in/xnnyygn/scala99/P00Test.scala
@@ -0,0 +1,11 @@
+package in.xnnyygn.scala99
+
+import org.specs2.mutable._
+
+// a template for test
+class P00Test extends Specification {
+  
+  "P00" should {
+    // test goes here
+  }
+}
